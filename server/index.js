a614a552add3cc206e34ffd3ea50369189b3b0c1
Pirkhs <nathan.briers32@gmail.com>
2025-01-16 18:08:11 +0000

    user password encryption

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
-
+import bcrypt from 'bcryptjs'
 import password from './password.js'
 import Card from '../models/card.js'
 import User from '../models/user.js'
@@ -20,14 +20,22 @@ mongoose.connect(CONNECTION_STRING)
 .catch(err => console.log(err))
 
 app.post('/api/packster/users', (req,res) => {
-    const doesUserExist = User.exists({ email: req.body.email })
+    User.exists({ email: req.body.email })
     .then(userExists => {
         if (userExists) throw new Error("User already exists with this email")
-        
-        const newUser = new User(req.body)
-        newUser.save()
-        .then(result => res.send(result))
     })
+    .then(() => {
+        return bcrypt.hash(req.body.password, 10)
+    })
+    .then(hash => {
+        const newUser = new User({
+            username: req.body.username,
+            password: hash,
+            email: req.body.email,
+        })
+        return newUser.save()
+    })
+    .then(result => res.send(result))
     .catch(err => console.log(err))
 })
 
